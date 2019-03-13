04d1143d04de148f1b0c44d8ed6eac2e2208ed8f
rafael <rgarcia.cti.com@gmail.com>
2019-03-13 07:55:03 +0100

    Multiple different files

---
+++ 05-memory/01.big_files.js
@@ -0,0 +1,25 @@
+
+
+const fs = require('fs');
+const baseString = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+
+/**
+ * Randomize array element order in-place.
+ * Using Durstenfeld shuffle algorithm.
+ */
+function shuffleArray(array) {
+  for (var i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+}
+
+const files = [...Array(10).keys()].map(i => fs.createWriteStream(`./big.file${i}`))
+
+for (let i = 0; i <= 1e6; i++) {
+  files.forEach(file => file.write(shuffleArray(baseString.split(' ')).join(' ') + '\n'))
+}
+
+file.end();
